60d41c5649e56385d243dc4cd3e988e1786ba94f
Matt Riley <m@ttriley.dev>
2023-04-15 17:14:03 +1000

    Unspecified changes

--- restruct.js
+++ restruct.js
@@ -1,31 +1,33 @@
-const restruct = (val, ref) => restruct.any(val, ref);
+module.exports = (val, ref, opt = { toggleName: 'enabled' }) => {
 
-restruct.any = (val, ref) => {
-    if (!val) return restruct.any([], ref);
-    if (Array.isArray(val)) return restruct.array(val, ref);
-    if (val.constructor === Object) return restruct.object(val, ref);
-    return restruct.any(Object.keys(ref), ref);
-};
-
-restruct.array = (arr, ref) => {
-    return restruct.any(Object.fromEntries(arr.map(el => {
-        if (el.constructor === String) return [el, true];
-        if (Array.isArray(el)) {
-            const [key, val] = el;
-            if (!val) return [key, false]
-            if (val.constructor === Object) return [key, val];
-            return [key, true]
+    const restruct = {
+        any: val => {
+            if (!val) return restruct.any([], ref);
+            if (Array.isArray(val)) return restruct.arr(val);
+            if (val.constructor === Object) return restruct.obj(val);
+            return restruct.any(Object.keys(ref));
+        },
+        arr: arr => {
+            return restruct.any(Object.fromEntries(arr.map(el => {
+                if (el.constructor === String) return [el, true];
+                if (Array.isArray(el)) {
+                    const [key, val] = el;
+                    if (!val) return [key, false]
+                    if (val.constructor === Object) return [key, val];
+                    return [key, true]
+                }
+            })));
+        },
+        obj: obj => {
+            return Object.fromEntries(Object.entries(ref).map(([key, refobj]) => {
+                const val = obj[key];
+                if (!val) return [key, { ...refobj, [opt.toggleName]: false }];
+                if (val.constructor === Object) return [key, { ...refobj, ...val, [opt.toggleName]: val.enabled ?? false }];
+                return [key, { ...refobj, [opt.toggleName]: true }];
+            }));
         }
-    })), ref);
-};
+    };
 
-restruct.object = (obj, ref, opt = { toggleName: 'enabled' }) => {
-    return Object.fromEntries(Object.entries(ref).map(([key, refobj]) => {
-        const val = obj[key];
-        if (!val) return [key, { ...refobj, [opt.toggleName]: false }];
-        if (val.constructor === Object) return [key, { ...refobj, ...val, [opt.toggleName]: val.enabled ?? false }];
-        return [key, { ...refobj, [opt.toggleName]: true }];
-    }));
-};
+    return restruct.any(val);
 
-module.exports = restruct;
+};
