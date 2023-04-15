e47d446963d5b11a53c9fdc002901b1c0f3ee259
Matt Riley <m@ttriley.dev>
2023-04-15 16:44:38 +1000

    Unspecified changes

--- restruct.js
+++ restruct.js
@@ -1,32 +1,36 @@
 const restruct = (val, ref) => {
 
-    if (!val) return restruct([], ref);
+    const recurse = val => {
+        if (!val) return recurse([], ref);
 
-    if (Array.isArray(val)) {
-        const obj = Object.fromEntries(val.map(el => {
-            if (typeof el === 'string') return [el, true];
-            if (Array.isArray(el)) {
-                const [key, val] = el;
-                if (!val) return [key, false]
-                if (val.constructor === Object) return [key, val];
-                return [key, true]
-            }
-        }));
+        if (Array.isArray(val)) {
+            const obj = Object.fromEntries(val.map(el => {
+                if (typeof el === 'string') return [el, true];
+                if (Array.isArray(el)) {
+                    const [key, val] = el;
+                    if (!val) return [key, false]
+                    if (val.constructor === Object) return [key, val];
+                    return [key, true]
+                }
+            }));
 
-        return restruct(obj, ref);
+            return recurse(obj, ref);
+        };
+
+        if (val.constructor === Object) {
+            const obj = val;
+            return Object.fromEntries(Object.entries(ref).map(([key, refobj]) => {
+                const val = obj[key];
+                if (!val) return [key, { ...refobj, enabled: false }];
+                if (val.constructor === Object) return [key, { ...refobj, ...val, enabled: val.enabled ?? false }];
+                return [key, { ...refobj, enabled: true }];
+            }));
+        }
+
+        return recurse(Object.keys(ref), ref);
     };
 
-    if (val.constructor === Object) {
-        const obj = val;
-        return Object.fromEntries(Object.entries(ref).map(([key, refobj]) => {
-            const val = obj[key];
-            if (!val) return [key, { ...refobj, enabled: false }];
-            if (val.constructor === Object) return [key, { ...refobj, ...val, enabled: val.enabled ?? false }];
-            return [key, { ...refobj, enabled: true }];
-        }));
-    }
-
-    return restruct(Object.keys(ref), ref);
+    return recurse(val);
 
 };
 
