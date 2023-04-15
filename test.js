6b690847f73f8b06d17949224aa84e168a48fd9f
Matt Riley <m@ttriley.dev>
2023-04-15 10:01:20 +1000

    Unspecified changes

--- test.js
+++ test.js
@@ -213,12 +213,44 @@ test('all enabled', () => {
     };
 
     const inputs = [
+        true,
+        undefined,
         ['foo', 'bar'],
         [['foo', true], ['bar', true]],
         [['foo', undefined], ['bar', undefined]],
         [['foo', { enabled: true }], ['bar', { enabled: true }]],
         { foo: true, bar: true },
         { foo: { enabled: true }, bar: { enabled: true } }
+    ];
+
+    inputs.forEach(input => {
+        const actual = restruct(input, ref);
+        assert.deepEqual(actual, expected);
+    });
+
+});
+
+test('all disabled', () => {
+    const expected = {
+        foo: {
+            enabled: false,
+            bar: 'foo',
+        },
+        bar: {
+            enabled: false,
+            foo: 'bar'
+        }
+    };
+
+    const inputs = [
+        false,
+        null,
+        [],
+        [['foo', false], ['bar', false]],
+        [['foo', null], ['bar', null]],
+        [['foo', { enabled: false }], ['bar', { enabled: false }]],
+        { foo: false, bar: false },
+        { foo: { enabled: false }, bar: { enabled: false } }
     ];
 
     inputs.forEach(input => {
