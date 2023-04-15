f54d54765d40bcf631664064be41f1131edf84cd
Matt Riley <m@ttriley.dev>
2023-04-15 16:49:52 +1000

    Unspecified changes

--- restruct.js
+++ restruct.js
@@ -1,4 +1,17 @@
-const restructArray = arr => {
+const restruct = (val, ref) => {
+
+    const recurse = val => {
+        if (!val) return recurse([]);
+        if (Array.isArray(val)) return recurse(restruct.array(val));
+        if (val.constructor === Object) return restruct.object(val, ref);
+        return recurse(Object.keys(ref));
+    };
+
+    return recurse(val);
+
+};
+
+restruct.array = arr => {
     return Object.fromEntries(arr.map(el => {
         if (typeof el === 'string') return [el, true];
         if (Array.isArray(el)) {
@@ -10,31 +23,13 @@ const restructArray = arr => {
     }))
 };
 
-const restructObject = (obj, ref) => {
+restruct.object = (obj, ref) => {
     return Object.fromEntries(Object.entries(ref).map(([key, refobj]) => {
         const val = obj[key];
         if (!val) return [key, { ...refobj, enabled: false }];
         if (val.constructor === Object) return [key, { ...refobj, ...val, enabled: val.enabled ?? false }];
         return [key, { ...refobj, enabled: true }];
     }));
-}
-
-module.exports = (val, ref) => {
-
-    const recurse = val => {
-        if (!val) return recurse([]);
-
-        if (Array.isArray(val)) {
-            return recurse(restructArray(val));
-        };
-
-        if (val.constructor === Object) {
-            return restructObject(val, ref);
-        }
-
-        return recurse(Object.keys(ref));
-    };
-
-    return recurse(val);
-
 };
+
+module.exports = restruct;
